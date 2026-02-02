4a5141e53a3076eb1c78c9ac3a5eee9589efe1eb
Tiago Pereira <clashfrog71@gmail.com>
2026-02-01 23:52:58 -0300

    adicionando o leitor de arquivo

--- ManipulaçãodeDados/Program.cs
+++ ManipulaçãodeDados/Program.cs
@@ -1,4 +1,5 @@
-﻿class Musica()
+﻿var arquivo = new FileStream("musicas.csv",FileMode.Open, FileAccess.Read);
+class Musica()
 {
     public string Nome { get; set; }
     public string Artista { get; set; }
