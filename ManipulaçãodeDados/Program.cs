89ae4486cd795e63e1a43c60501c9bfd3912c6d3
Tiago Pereira <clashfrog71@gmail.com>
2026-02-02 22:11:49 -0300

    Melhorando metodo de ler as musicas e adicionando e implementando metodo de exibir as musicas

--- ManipulaçãodeDados/Program.cs
+++ ManipulaçãodeDados/Program.cs
@@ -1,4 +1,37 @@
-﻿var arquivo = new FileStream("musicas.csv",FileMode.Open, FileAccess.Read);
+﻿using var arquivo = new FileStream("C:\\Users\\clash\\source\\repos\\ManipulaçãodeDados\\ManipulaçãodeDados\\musicas.csv", FileMode.Open, FileAccess.Read);
+using var leitor = new StreamReader(arquivo);
+
+var musicas = Obtermusicas(leitor);
+exibirMusicas(musicas);
+void exibirMusicas(IEnumerable<Musica> musicas)
+{
+    foreach (var musica in musicas)
+    {
+        var contador = 0;
+        Console.WriteLine($"Musica: {musica.Nome}");
+        contador++;
+        if(contador >= 10)
+            break;
+    }
+}
+IEnumerable<Musica> Obtermusicas(StreamReader leitor)
+{
+    var linha = leitor.ReadLine();
+    while (linha is not null)
+    {
+        var dados = linha.Split(';');
+        yield return new Musica
+        {
+            Nome = dados[0],
+            Artista = dados[1],
+            Duracao = int.Parse(dados[2])
+        };
+        linha = leitor.ReadLine();
+        
+
+    }
+}
+
 class Musica()
 {
     public string Nome { get; set; }
